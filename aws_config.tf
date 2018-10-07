197ebef06485e190355361bbd4c1ec0132884a79
pradeepkumartangudu <pradeepkumartangudu@gmail.com>
2018-10-07 06:43:05 +0530

    Update aws_config.tf

--- aws_config.tf
+++ aws_config.tf
@@ -7,6 +7,8 @@
 # Provider
 #
 provider "aws" {
+  access_key = "AKIAIUBQGDAIZRDJZOFQ"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
@@ -19,6 +21,6 @@ terraform {
     bucket = "mysoft-test-us-east-1"
     region = "us-east-1"
 	encrypt = true
-    kms_key_id    = "arn:aws:kms:us-east-1:661856373316:key/625b2a7c-ffc1-4a92-84bc-eace2f49b8f0"
+    kms_key_id    = "arn:aws:kms:us-east-1:661856373316:key/807d6be7-fc22-444b-90d4-50ca73588f9a"
   }
 }
