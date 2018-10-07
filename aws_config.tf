84ebb195fbba77d8bd9f86b0fb2bd591c4d0371f
pradeepkumartangudu <pradeepkumartangudu@gmail.com>
2018-10-07 05:46:13 +0530

    Update aws_config.tf

--- aws_config.tf
+++ aws_config.tf
@@ -17,7 +17,6 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "mysoft-test-us-east-1"
-    key    = "runtime/${bucketname}/terraform.tfstate"
     region = "us-east-1"
 	encrypt = true
     kms_key_id    = "arn:aws:kms:us-east-1:661856373316:key/625b2a7c-ffc1-4a92-84bc-eace2f49b8f0"
